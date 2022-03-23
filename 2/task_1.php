33a619a320fd8f633405a6053ae8bee80fa29083
zabulyaka <zabulyaka@gmail.com>
2022-03-23 13:32:50 +0500

    Added 2nd homework

---
+++ 2/task_1.php
@@ -0,0 +1,14 @@
+<?php
+echo "
+	<div style='background-color:#f2f2f2;width: 500px'> 
+		<p style='text-align: center'>Внимание!<p>
+		<p>Мы становимся опытными программистами. Изучив язык PHP, мы сможем создавать:</p>
+		<ul style='padding-left: 15px'>
+			<li>гостевые книги,</li>
+			<li>форумы,</li>
+			<li>чаты</li>
+			<li>и управлять целыми портальными системами.<br /> Мы сделаем это!</li>
+		</ul>
+	</div>
+	";
+?>
---
+++ 2/task_2.php
@@ -0,0 +1,6 @@
+<?php
+$a=-2;
+if($a<3 && $a>0) echo 'Hello world';
+else if($a==3) echo 'Hi world';
+else echo 'Goodbye world';
+?>
